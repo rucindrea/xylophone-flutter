38ae28f5f39c7a33121b89e6e103b8bb86fc03a9
Ru Cindrea <ru.cindrea@altom.com>
2020-09-08 07:25:39 +0300

    Implement notes playing in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  Expanded buildKey(int note, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(note);
+        },
+        child: Text('Note $note'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.blue),
+              buildKey(6, Colors.indigo),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
